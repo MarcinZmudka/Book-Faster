5f2886079e412f001dad07a50e85fb31fde1fe49
unknown <marcinzmudka@gmail.com>
2019-09-16 15:34:52 +0200

    deploy heroku

---
+++ public/precache-manifest.15f3b97c9d6197a16951ade9289e8919.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a329fdc7097c249cacbe677658e494ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88c66feb0e833e83558a",
+    "url": "/static/css/main.cd764a43.chunk.css"
+  },
+  {
+    "revision": "62c60bf37838070a96cd",
+    "url": "/static/js/2.a67ce1e3.chunk.js"
+  },
+  {
+    "revision": "88c66feb0e833e83558a",
+    "url": "/static/js/main.92043f99.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9c4228143b85af9c632f949f5fba30e",
+    "url": "/static/media/Statystyki.b9c42281.png"
+  },
+  {
+    "revision": "e8931ac38ffb9417171f15df4bf4a25d",
+    "url": "/static/media/booking.e8931ac3.png"
+  },
+  {
+    "revision": "9a21bd82872e73dc75b1bd90f7b97ce8",
+    "url": "/static/media/dane-maxi.9a21bd82.png"
+  },
+  {
+    "revision": "96cb4a2c77bb9d8d35631642bd93f13f",
+    "url": "/static/media/dane-mini.96cb4a2c.png"
+  },
+  {
+    "revision": "20f7724657a17460e25fe0cf85603072",
+    "url": "/static/media/wyszukiwarka.20f77246.png"
+  }
+]);
